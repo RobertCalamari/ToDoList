f1ec63ea91a2c9df41e08fbfce838f078a987af2
RobertCalamari <36252593+RobertCalamari@users.noreply.github.com>
2018-05-09 04:53:09 -0400

    add and delete both work

--- src/TodoList.js
+++ src/TodoList.js
@@ -12,12 +12,18 @@ class TodoList extends Component {
 
 		this.addItem = this.addItem.bind(this);
 		this.deleteItem = this.deleteItem.bind(this);
-		this.subItem = this.subItem.bind(this);
+		//this.subItem = this.subItem.bind(this);
 	}
 
 	addItem(e) {
 
 
+		var radioB1 = document.getElementById('rad1');
+		var radioB2 = document.getElementById('rad2');
+		var radioB3 = document.getElementById('rad3');
+
+
+		if(radioB1.checked==true){
 
 			if(this._inputElement.value !== "")
 			{
@@ -34,33 +40,24 @@ class TodoList extends Component {
 			}
 
 			this._inputElement.value = "";
+		}
 
 			e.preventDefault();
 		
 	}
 
 	deleteItem(key) {
-		var filteredItems = this.state.items.filter(function (item) {
-		return (item.key !== key)
-	});
 
-		this.setState({
-			items:filteredItems
-		});
-	}
- 
-	subItem(e) {
 
 		var radioB1 = document.getElementById('rad1');
 		var radioB2 = document.getElementById('rad2');
 		var radioB3 = document.getElementById('rad3');
 
 
-		if(radioB1.checked == "checked")
-		{
-			
-		}
-				if(this._inputElement.value !== "")
+		if(radioB2.checked==true){
+
+
+			if(this._inputElement.value !== "")
 			{
 				var newItem = {
 					text: this._inputElement.value,
@@ -74,26 +71,87 @@ class TodoList extends Component {
 				});
 			}
 
-			this._inputElement.value = "";
+			// var filteredItems = this.state.items.filter(function (item) {
+			// 	return (item.key !== key)
+			// 	});
 
-			e.preventDefault();
+			// 	this.setState({
+			// 		items:filteredItems
+			// 	});
+					
+			// this._inputElement.value = "";
+
+
+
+
+
+		}
+		else
+		{
+
+			var filteredItems = this.state.items.filter(function (item) {
+				return (item.key !== key)
+				});
+
+				this.setState({
+					items:filteredItems
+				});
+
+				
+
+		}
+	}
+ 
+	// subItem(e) {
+
+	// 	var radioB1 = document.getElementById('rad1');
+	// 	var radioB2 = document.getElementById('rad2');
+	// 	var radioB3 = document.getElementById('rad3');
+
+
+	// 	if(radioB1.checked==true){
+
+	// 		if(this._inputElement.value !== "")
+	// 		{
+
+	// 			var newItem = {
+	// 				text: this._inputElement.value,
+	// 				key: Date.now()
+	// 			};
+
+	// 			this.setState((prevState) => {
+	// 				return{
+	// 					items: prevState.items.concat(newItem)
+	// 				};
+	// 			});
+		
+
+	// 		}
+
+	// 		this._inputElement.value = "";
+
+	// 		}
+
+
+	// 		e.preventDefault();
 
 	
 
-	}
+	// }
 
 	render() {
 		return (
 
 			<div className="todoListMain">
 				<div className="header">
-					<form onSubmit={this.subItem}>
+					Add <input type="radio" id="rad1" name="sub" value="add" checked="checked" /><br/>
+						Edit<input type="radio" id="rad2" name="sub" value="edit"/><br/>
+						Del<input type="radio" id="rad3" name="sub" value="delete"/>
+					<form onSubmit={this.addItem}>
 						<input id="inptask" ref={(a) => this._inputElement = a} placeholder="Enter Task"></input>
 						<button type="submit" id="addbut">Submit</button>
 					</form>
-						Add <input type="radio" id="rad1" name="sub" value="add" checked="checked"/><br/>
-						Edit<input type="radio" id="rad2" name="sub" value="edit"/><br/>
-						Del<input type="radio" id="rad3" name="sub" value="delete"/>
+						
 						
 				</div>
 				<TodoItems entries={this.state.items} delete={this.deleteItem}/>
